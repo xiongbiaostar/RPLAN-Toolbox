029f194388695947016c0ce7663a990477d1a9d7
EscaeP <escae123@163.com>
2024-09-19 11:41:24 +0800

    json

--- RPLAN-Toolbox-master/test.py
+++ RPLAN-Toolbox-master/test.py
@@ -8,6 +8,7 @@ from rplan.decorate import get_dw
 from rplan.measure import compute_tf
 from rplan.plot import get_figure,get_axes,plot_category,plot_boundary,plot_graph,plot_fp,plot_tf
 import  output_class
+import manager
 
 RPLAN_DIR = './data'
 file_path = f'{RPLAN_DIR}/0.png'
@@ -23,7 +24,6 @@ data['boxes_aligned'] = boxes_aligned
 data['order'] = order
 data['room_boundaries'] = room_boundaries
 
-print(room_boundaries,'\n')
 
 doors,windows = get_dw(data)
 data['doors'] = doors
@@ -41,52 +41,46 @@ def convert_numpy_to_list(obj):
     else:
         return obj
 
+json_file_path = './output/test.json'
+_manager = manager.project_setting_manager(json_file_path)
+
 _Rooms = []
 rooms=fp._get_rooms()
 for room in rooms:
     _Room = output_class.Room(room).to_dict()
     _Rooms.append(_Room)
 _Room_dict_serializable = convert_numpy_to_list(_Rooms)
-_Room_json = json.dumps(_Room_dict_serializable,indent=4)
-with open('output/room.json', 'w') as f:
-    f.write(_Room_json)
+_manager.add_data('rooms',_Room_dict_serializable)
 
 _Windows = []
 for window in windows:
     _Window = output_class.Window(window).to_dict()
     _Windows.append(_Window)
 _Window_dict_serializable = convert_numpy_to_list(_Windows)
-_Window_json = json.dumps(_Window_dict_serializable,indent=4)
-with open('output/window.json', 'w') as f:
-    f.write(_Window_json)
+_manager.add_data('windows',_Window_dict_serializable)
 
-#door type : 0(door) / 1(open wall)
 _Doors = []
 for door in doors:
     _Door = output_class.Door(door).to_dict()
     _Doors.append(_Door)
 _Door_dict_serializable = convert_numpy_to_list(_Doors)
-_Door_json = json.dumps(_Door_dict_serializable,indent=4)
-with open('output/door.json', 'w') as f:
-    f.write(_Door_json)
+_manager.add_data('doors',_Door_dict_serializable)
 
 _Edges = []
 for edge in edges:
     _Edge = output_class.Edge(edge).to_dict()
     _Edges.append(_Edge)
 _Edge_dict_serializable = convert_numpy_to_list(_Edges)
-_Edge_json = json.dumps(_Edge_dict_serializable,indent=4)
-with open('output/edge.json', 'w') as f:
-    f.write(_Edge_json)
+_manager.add_data('edges',_Edge_dict_serializable)
 
 _Walls = []
 for wall in room_boundaries:
     _Wall = output_class.Wall(wall).to_dict()
     _Walls.append(_Wall)
 _Wall_dict_serializable = convert_numpy_to_list(_Walls)
-_Wall_json = json.dumps(_Wall_dict_serializable,indent=4)
-with open('output/wall.json', 'w') as f:
-    f.write(_Wall_json)
+_manager.add_data('walls',_Wall_dict_serializable)
+
+_manager.save()
 
 
 #fig = get_figure([512,512])
